40ac0c477b11d16fabd7edb7c82c30c285458049
Jannis Braun <jannis.braun@bluewin.ch>
2025-05-29 04:40:03 +0200

    solved

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :restaurants, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:index, :new, :create]
-  end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-Restaurant.destroy_all
-Review.destroy_all
-
-restaurant = Restaurant.create(name: "rest A", address: "A street", category: "japanese")
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-
-restaurant = Restaurant.create(name: "rest B", address: "B street", category: "japanese")
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-
-restaurant = Restaurant.create(name: "rest C", address: "C street", category: "italian")
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-
-restaurant = Restaurant.create(name: "rest D", address: "D street", category: "french")
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-
-restaurant = Restaurant.create(name: "rest E", address: "E street", category: "belgian")
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
-Review.create(content: "pizza" , rating: rand(0..5), restaurant: restaurant)
